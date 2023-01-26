29b44791b6eb170f01e075112159afd8d29822a7
EC2 Default User <ec2-user@ip-172-31-35-255.ap-northeast-1.compute.internal>
2023-01-26 11:32:40 +0000

    fix2.item/tagマイグレーション

--- db/migrate/20230121110003_create_item_tags.rb
+++ db/migrate/20230121110003_create_item_tags.rb
@@ -1,8 +1,8 @@
 class CreateItemTags < ActiveRecord::Migration[6.1]
   def change
     create_table :item_tags do |t|
-      t.references :item, null: false, foreign_key: true
-      t.references :tag, null: false, foreign_key: true
+      t.bigint :item, null: false, foreign_key: true
+      t.bigint :tag, null: false, foreign_key: true
 
       t.timestamps
     end
