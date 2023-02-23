8650c1f66b76a05967f78ca739f0e3b6531a7c11
EC2 Default User <ec2-user@ip-172-31-35-255.ap-northeast-1.compute.internal>
2023-02-23 13:37:03 +0000

    新規投稿・編集 部分テンプレート化

--- db/schema.rb
+++ db/schema.rb
@@ -89,7 +89,9 @@ ActiveRecord::Schema.define(version: 2023_01_21_110402) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "name", null: false
+    t.string "namespace :`Filename()` do
+      
+    ende", null: false
     t.boolean "is_deleted", default: false, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
